b2186a63532a8bb365da67ae45e4599990b5dbf6
Rasul Tuleev <amp.rasul@mail.ru>
2026-07-13 18:16:51 +0600

    Fix import for webhook

--- birge/urls.py
+++ birge/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from api import views as api_views   # <-- ЭТА СТРОЧКА ДОБАВЛЕНА
 
 urlpatterns = [
     path('admin/', admin.site.urls),
