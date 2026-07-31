caee0861b790a8f706e600d457da7545b75cf4a7
Rasul Tuleev <amp.rasul@mail.ru>
2026-07-31 13:41:42 +0600

    Добавлен вызов ensure_admin при старте wsgi

--- birge/wsgi.py
+++ birge/wsgi.py
@@ -14,3 +14,17 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'birge.settings')
 
 application = get_wsgi_application()
+
+# Вызов ensure_admin при старте wsgi
+try:
+    from django.core.management import call_command
+    call_command('ensure_admin')
+except Exception as e:
+    print(f'Error ensuring admin: {e}')
+
+# Вызов ensure_admin при старте wsgi
+try:
+    from django.core.management import call_command
+    call_command('ensure_admin')
+except Exception as e:
+    print(f'Error ensuring admin: {e}')
